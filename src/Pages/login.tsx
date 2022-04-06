65e701d9e7a8826eae31502e9afcd26d7bc3c0c9
Sayah Reda <reda1998.rs@gmail.com>
2022-04-07 00:28:10 +0100

    feat: add images ads to login page

--- src/Pages/login.tsx
+++ src/Pages/login.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Form_Login from '../component/Form_Login';
+import review from '../imgs/ads/reviews.png';
+import order from '../imgs/ads/order.png';
+import secorder from '../imgs/ads/secorder.png';
 
 function Login() {
   return (
-    <div className="flex">
+    <div className="flex h-[100vh]">
       <Form_Login />
-      <div>HEllo to test</div>
+      <div className="bg-[#697BFF] w-[55%]">
+        <div className="flex items-center justify-center mt-16">
+          {' '}
+          <img src={review} alt="review" />
+          <img src={secorder} alt="order preview" />{' '}
+        </div>
+        <img
+          className="flex  justify-center px-48"
+          src={order}
+          alt="order preview second"
+        />
+      </div>
     </div>
   );
 }
