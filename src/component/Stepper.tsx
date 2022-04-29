4bd099c3c67af519ced69cc4cf2f0c1ba8cd9248
Sayah Reda <reda1998.rs@gmail.com>
2022-04-29 23:54:50 +0100

    highlight step when passing

--- src/component/Stepper.tsx
+++ src/component/Stepper.tsx
@@ -64,24 +64,24 @@ return newSteps;
         })
     })
     stepsRef.current=stepState;
-    const current=updateStep(props.currentStep-1,stepsRef.current);
+    const current=updateStep(props.currentStep-1,stepsRef.current); 
     setNewStep(current)
 
   },[props.steps,props.currentStep])
 
   return (
-<div className='flex justify-between py-6'>
+<div className='flex justify-end py-20'>
 {newStep.map((step:IStepref,index:number)=>{
 return(
-  <div key={index} className="mx-4 p-8 flex justify-between items-center">
+  <div key={index} className="  flex justify-between items-center w-36">
 
   {}
-    <div className='relative flex flex-col items-center'>
-        <div className=' rounded-full transition duration-500 ease-in-out h-12 w-12 flex items-center justify-center py-3 border-gray-300 border-2'>{index+1}</div>
+    <div className={"relative flex flex-col items-center  "}>
+        <div className={` rounded-full transition duration-500 ease-in-out h-12 w-12 flex items-center justify-center py-3 border-gray-300 border-2 ${step.selected ? "bg-[#697bff] border-[#697bff] text-white" : ""}`}>{step.completed ? (<span className='text-white font-bold text-xl'>&#10003;</span>):index+1}</div>
         <div className='absolute top-0 text-center mt-16 w-32 text-base font-medium uppercase'>{step.Description}</div>
     </div>
-    <div className="flex-auto  border-t-2 trasition duration-500 ease-in-out"></div>
-  
+    {index !==newStep.length-1 && <div className="flex-auto w-20 border-t-2 trasition duration-500 ease-in-out"></div>
+  } 
    
   
   
