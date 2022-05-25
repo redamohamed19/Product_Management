fe6a9d9dc983259fc3d3dee587351a43bf8f0662
Sayah Reda <reda1998.rs@gmail.com>
2022-05-25 11:18:43 +0100

    Add:Add marketing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Login from './Pages/login';
-//import Home from './Pages/home'; 
+import Home from './Pages/home'; 
 
 import Settings from './Pages/Settings';
 import './App.css';
@@ -9,7 +9,7 @@ import Sign_Up from './Pages/Sign_Up';
 function App() {
   return (
     <div>
-     <Sign_Up/>
+     <Home/>
     </div>
   );
 }
---
+++ src/Pages/Marketing.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Marketing() {
+  return (
+    <div className="flex h-[100vh]">
+
+    </div>
+  );
+}
+
+export default Marketing;
