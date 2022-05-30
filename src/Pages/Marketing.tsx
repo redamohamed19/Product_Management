a804451aa4a541e85d8a55c16059d2315fb595f0
Sayah Reda <reda1998.rs@gmail.com>
2022-05-30 23:56:27 +0100

    Update:mArekting Page Design

--- src/Pages/Marketing.tsx
+++ src/Pages/Marketing.tsx
@@ -12,11 +12,11 @@ function Marketing() {
   return (
     <div className=" h-[100vh]">
 <NavBarNoAutothentificated/>
-<div className='flex  px-0 py-32  lg:px-4 sm:pr-8 justify-around sm:flex-col sm:gap-32 '>
-  <div className='w-2/5 sm:w-full'>
+<div className='flex  px-0 py-32 sssm:py-10 sm:py-4 lg:px-16 sm:pr-8 justify-around lg:flex-col lg:gap-32 '>
+  <div className='w-2/5 lg:w-full '>
 
-    <h1 className=' text-6xl font-bold py-10 '>We're <span className='text-[#4E60FF] '>Serious</span> For <span className='text-[#4E60FF]'>Food</span> And Delivery </h1>
-    <p className=' text-2xl py-3'>
+    <h1 className=' text-6xl font-bold py-10 sm:text-[50px] sm:text-center  sssm:text-4xl leading-20  sssm:py-3'>We're <span className='text-[#4E60FF] '>Serious</span> For <span className='text-[#4E60FF]'>Food</span> And Delivery </h1>
+    <p className=' text-2xl py-3 sssm:text-xl sm:text-center lg:w-4/5'>
     Best cooks and best delivery guys all at your service. Hot tasty food will reach you in 60 minutes.
     </p>
     <form>
@@ -28,15 +28,15 @@ function Marketing() {
       </div>
     </form>
   </div>
-  <div className='w-2/5 relative flex justify-center items-center sm:w-full ssm:hidden'>
+  <div className='w-2/5 relative flex justify-center items-center lg:w-full lg:hidden'>
     <div className=''>
-    <img className='plat ' src={Plat01} alt="plat"/>
-    <img className='plat ' src={Plat02} alt="plat"/>
-    <img className='plat ' src={Plat03} alt="plat"/>
-    <img className='plat ' src={Plat04} alt="plat"/>
+    <img className='plat xl:w-32  xl:h-32' src={Plat01} alt="plat"/>
+    <img className='plat xl:w-32  xl:h-32' src={Plat02} alt="plat"/>
+    <img className='plat xl:w-32  xl:h-32' src={Plat03} alt="plat"/>
+    <img className='plat xl:w-32  xl:h-32' src={Plat04} alt="plat"/>
     </div>
     <div  className='Banner '>
-      <img src={Banner} alt="Banner"/>
+      <img className='xl:w-64 ' src={Banner} alt="Banner"/>
     </div>
   </div>
 </div>
