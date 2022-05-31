24ab6e9a969802cc32e59cc9abe6464abac953e7
Sayah Reda <reda1998.rs@gmail.com>
2022-05-31 15:40:56 +0100

    Update:Banner design

--- src/Pages/Marketing.tsx
+++ src/Pages/Marketing.tsx
@@ -2,21 +2,17 @@ import React from 'react';
 import Search from "../imgs/icons/Search_White.svg"
 import Banner from "../imgs/ads/banner.png"
 import NavBarNoAutothentificated from '../component/NavBarNoAutothentificated';
-import Plat01 from "../imgs/plat/plat01.png"
-import Plat02 from "../imgs/plat/plat02.png"
-import Plat03 from "../imgs/plat/plat03.png"
-import Plat04 from "../imgs/plat/plat04.png"
 
 
 function Marketing() {
   return (
     <div className=" h-[100vh]">
 <NavBarNoAutothentificated/>
-<div className='flex  px-0 py-32 sssm:py-10 sm:py-4 lg:px-16 sm:pr-8 justify-around lg:flex-col lg:gap-32 '>
+<div className='flex  px-16 py-12 sssm:py-10 sssm:px-4 sm:py-4 lg:px-8 sm:pr-8 justify-around items-center lg:flex-col lg:gap-32 '>
   <div className='w-2/5 lg:w-full '>
 
-    <h1 className=' text-6xl font-bold py-10 sm:text-[50px] sm:text-center  sssm:text-4xl leading-20  sssm:py-3'>We're <span className='text-[#4E60FF] '>Serious</span> For <span className='text-[#4E60FF]'>Food</span> And Delivery </h1>
-    <p className=' text-2xl py-3 sssm:text-xl sm:text-center lg:w-4/5'>
+    <h1 className=' text-6xl font-bold py-10 sm:text-[50px]   sssm:text-4xl leading-20  sssm:py-3 '>We're <span className='text-[#4E60FF] '>Serious</span> For <span className='text-[#4E60FF]'>Food</span> And Delivery </h1>
+    <p className=' text-2xl py-3 sssm:text-lg '>
     Best cooks and best delivery guys all at your service. Hot tasty food will reach you in 60 minutes.
     </p>
     <form>
@@ -28,15 +24,10 @@ function Marketing() {
       </div>
     </form>
   </div>
-  <div className='w-2/5 relative flex justify-center items-center lg:w-full lg:hidden'>
-    <div className=''>
-    <img className='plat xl:w-32  xl:h-32' src={Plat01} alt="plat"/>
-    <img className='plat xl:w-32  xl:h-32' src={Plat02} alt="plat"/>
-    <img className='plat xl:w-32  xl:h-32' src={Plat03} alt="plat"/>
-    <img className='plat xl:w-32  xl:h-32' src={Plat04} alt="plat"/>
-    </div>
-    <div  className='Banner '>
-      <img className='xl:w-64 ' src={Banner} alt="Banner"/>
+  <div className='w-2/5 relative flex justify-center items-center lg:w-full lg:hidden '>
+
+    <div  className=' '>
+      <img className='w-[450px] max-w-none 2xl:w-[550px]  ' src={Banner} alt="Banner"/>
     </div>
   </div>
 </div>
